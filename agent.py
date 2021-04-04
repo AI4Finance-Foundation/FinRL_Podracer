e29ca60a0c932e1668c8d15666ac1ab009a4c3ac
Steven Li <46941629+supersglzc@users.noreply.github.com>
2021-04-04 15:36:31 +0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -4,10 +4,10 @@ from copy import deepcopy  # deepcopy target_network
 import torch
 import numpy as np
 import numpy.random as rd
-from elegantrl.net import QNet, QNetDuel, QNetTwin, QNetTwinDuel
-from elegantrl.net import Actor, ActorSAC, ActorPPO
-from elegantrl.net import Critic, CriticAdv, CriticTwin
-from elegantrl.net import InterDPG, InterSPG, InterPPO
+from net import QNet, QNetDuel, QNetTwin, QNetTwinDuel
+from net import Actor, ActorSAC, ActorPPO
+from net import Critic, CriticAdv, CriticTwin
+from net import InterDPG, InterSPG, InterPPO
 
 
 class AgentBase:
