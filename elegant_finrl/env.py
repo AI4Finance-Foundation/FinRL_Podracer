8312f47db26bc4b3576df9f2f58d03be2e73c918
Steven Li <46941629+supersglzc@users.noreply.github.com>
2021-05-06 15:43:06 +0800

    Update env.py

--- elegant_finrl/env.py
+++ elegant_finrl/env.py
@@ -13,6 +13,7 @@ class StockTradingEnv:
                  start_date='2009-01-01', end_date='2019-01-01', env_eval_date='2021-01-01',
                  ticker_list=None, tech_indicator_list=None, initial_stocks=None, reward_scaling=2 ** -14, if_eval=False):
 
+        self.date_list = None
         self.price_ary, self.tech_ary = self.load_data(cwd, if_eval, ticker_list, tech_indicator_list,
                                                        start_date, end_date, env_eval_date, )
         stock_dim = self.price_ary.shape[1]
